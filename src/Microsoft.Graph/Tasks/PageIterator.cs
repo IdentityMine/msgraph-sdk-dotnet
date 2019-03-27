b3ece5d5af848593e3e35da14e0d38edb3d5e826
Michael Mainer <mmainer@microsoft.com>
2019-03-26 17:08:29 -0700

    Added iterator tests

---
+++ src/Microsoft.Graph/Tasks/PageIterator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Threading.Tasks;
+
+/**
+
+Spec
+    https://github.com/microsoftgraph/msgraph-sdk-design/blob/master/tasks/PageIteratorTask.md
+**/
+
+namespace Microsoft.Graph
+{
+    /// <summary>
+    /// Use PageIterator&lt;T&gt; to automatically page through result sets across multiple calls 
+    /// and process each item in the result set.
+    /// </summary>
+    /// <typeparam name="T">The common entity type returned in the result set.</typeparam>
+    public class PageIterator<T> where T : Entity
+    {
+        private ICollectionPage<T> initialPage;
+        private Func<T, bool> processPageItem;
+
+        /// <summary>
+        /// Creates the PageIterator with the results of an initial paged request. 
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="processPageItems">A Func delegate that processes type T in the result set and should return false if the iterator should cancel processing.</param>
+        /// <returns>A PageIterator&lt;T&gt; that will process additional result pages based on the rules specified in Func&lt;T,bool&gt; processPageItems</returns>
+        public static PageIterator<T> CreatePageIterator(ICollectionPage<T> page, Func<T,bool> processPageItems)
+        {
+            if (page == null)
+                throw new ArgumentNullException("page");
+
+            if (processPageItems == null)
+                throw new ArgumentNullException("processPageItems");
+
+            return new PageIterator<T>()
+            {
+                initialPage = page,
+                processPageItem = processPageItems
+            };
+        }
+
+        /// <summary>
+        /// Fetches page collections and iterates through each page of items and processes it according to the Func&lt;T, bool&gt; set in <see cref="CreatePageIterator"/>. 
+        /// </summary>
+        /// <returns>The task object that represents the results of this asynchronous operation.</returns>
+        /// <exception cref="Microsoft.CSharp.RuntimeBinder.RuntimeBinderException">Thrown when a base CollectionPage does not implement NextPageRequest.
+        /// is provided to the PageIterator</exception>
+        public async Task IterateAsync()
+        {
+            // We need access to the NextPageRequest to call and get the next page. ICollectionPage<T> doesn't define NextPageRequest.
+            // We are making this dynamic so we can access NextPageRequest.
+            dynamic page = initialPage;
+
+            bool shouldFetchMorePages = true; // Set false if no more pages to fetch or if processPageItem() returns false
+
+            do
+            {
+                // Process each item in a page.
+                foreach (T item in page)
+                {
+                    bool shouldContinue = processPageItem(item);
+
+                    // Cancel processing of items in the page and stop requesting more pages.
+                    if (!shouldContinue)
+                    {
+                        shouldFetchMorePages = false;
+                        break;
+                    }
+                }
+
+                // Fetch the next page of results. RuntimeBinderException expection can be thrown if page is a base CollectionPage object.
+                if (page.NextPageRequest != null && shouldFetchMorePages)
+                {
+                    page = await page.NextPageRequest.GetAsync();
+                }
+                else
+                {
+                    shouldFetchMorePages = false;
+                }
+            } while (shouldFetchMorePages);
+        }
+    }
+}
---
+++ tests/Microsoft.Graph.DotnetCore.Test/Tasks/PageIteratorTests.cs
@@ -0,0 +1,102 @@
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+using Microsoft.CSharp.RuntimeBinder;
+using Microsoft.Graph.DotnetCore.Test.Requests.Functional;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Graph.DotnetCore.Test.Tasks
+{
+    public class PageIteratorTests : GraphTestBase
+    {
+        private PageIterator<Event> pageIterator;
+        
+        [Fact]
+        public async Task PageIteratorDevTest()
+        {
+            // Get an initial page results to populate the iterator.
+            IUserEventsCollectionPage iUserEventsCollectionPage = await graphClient.Me.Events.Request().Top(2).GetAsync();
+            
+            // Create the function to process each entity returned in the pages
+            Func<Event,bool> processUser = (e) =>
+            {
+                bool shouldContinue = true;
+
+                if (e.Subject.Contains("Latin"))
+                    shouldContinue = false;
+                Debug.WriteLine($"Event subject: {e.Subject}");
+                return shouldContinue;
+            }; 
+
+            // This requires the dev to specify the generic type in the CollectionPage.
+            pageIterator = PageIterator<Event>.CreatePageIterator(iUserEventsCollectionPage, processUser);
+
+            await pageIterator.IterateAsync();
+        }
+
+        [Fact]
+        public async Task Given_Concrete_CollectionPage_It_Throws_RuntimeBinderException()
+        {
+            pageIterator = PageIterator<Event>.CreatePageIterator(new CollectionPage<Event>(), (e) => { return true; });
+            await Assert.ThrowsAsync<RuntimeBinderException>(() => pageIterator.IterateAsync());
+        }
+
+        [Fact]
+        public async Task Given_Concrete_Generated_CollectionPage_It_Iterates_Page_Items()
+        {
+            int inputEventCount = 17;
+            var page = new UserEventsCollectionPage();
+            for (int i = 0; i < inputEventCount; i++)
+            {
+                page.Add(new Event() { Subject = $"Subject{i.ToString()}" });
+            }
+
+            List<Event> events = new List<Event>();
+
+            pageIterator = PageIterator<Event>.CreatePageIterator(page, (e) => 
+            {
+                events.Add(e);
+                return true;
+            });
+
+            await pageIterator.IterateAsync();
+
+            Assert.Equal(inputEventCount, events.Count);
+        }
+
+        [Fact]
+        public async Task Given_Concrete_Generated_CollectionPage_It_Stops_Iterating_Pageitems()
+        {
+            int inputEventCount = 17;
+            var page = new UserEventsCollectionPage();
+            for (int i = 0; i < inputEventCount; i++)
+            {
+                page.Add(new Event() { Subject = $"Subject{i.ToString()}" });
+            }
+
+            List<Event> events = new List<Event>();
+
+            pageIterator = PageIterator<Event>.CreatePageIterator(page, (e) =>
+            {
+                if (e.Subject == "Subject7")
+                    return false;
+                
+                events.Add(e);
+                return true;
+            });
+
+            await pageIterator.IterateAsync();
+
+            Assert.Equal(7, events.Count);
+        }
+
+        // Given_Concrete_Generated_CollectionPage_It_Stops_Iterating_Across_Pages
+        // Given_Concrete_Generated_CollectionPage_It_Iterates_Across_Pages
+        // Given_Concrete_Generated_Hetero_CollectionPage_It
+    }
+}
